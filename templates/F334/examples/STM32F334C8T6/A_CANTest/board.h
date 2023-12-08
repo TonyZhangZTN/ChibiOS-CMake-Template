dcdfe60068de02d7589d37a45fdf314f23e1b1a0
TonyZhang <mcerztn@gmail.com>
2023-12-08 23:07:34 +0800

    switch to HSE

--- templates/F334/examples/STM32F334C8T6/A_CANTest/board.h
+++ templates/F334/examples/STM32F334C8T6/A_CANTest/board.h
@@ -48,7 +48,7 @@
 #define STM32_LSEDRV                (3U << 3U)
 
 #if !defined(STM32_HSECLK)
-#define STM32_HSECLK                0U
+#define STM32_HSECLK                27000000U
 #endif
 
 #define STM32_HSE_BYPASS
